5ec632ffa7a2082ce06a379abdba7840fef55d3c
Filippo Scelfo <scelfofilippo@gmail.com>
2023-01-08 20:06:14 +0100

    Clean up code for app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,8 @@ import "express-async-errors";
 const app = express();
 
 app.get("/planets", (request, response) => {
+  //  response.send("Up and running!");
   response.json([{ name: "Jupiter" }, { name: "Mars" }]);
 });
-
-// app.get("/", (request, response) => {
-//   response.send("Up and running!");
-// });
 
 export default app;
