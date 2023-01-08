660d3bca89cbaee9eb8c761b322d89039f9875af
Filippo Scelfo <scelfofilippo@gmail.com>
2023-01-08 20:02:55 +0100

    Add GET /planets root

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,12 @@ import "express-async-errors";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send("Up and running!");
+app.get("/planets", (request, response) => {
+  response.json([{ name: "Jupiter" }, { name: "Mars" }]);
 });
+
+// app.get("/", (request, response) => {
+//   response.send("Up and running!");
+// });
 
 export default app;
