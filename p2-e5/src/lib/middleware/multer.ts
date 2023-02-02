463d1c948863833c96abbaa6873f1186d6537f42
Filippo Scelfo <scelfofilippo@gmail.com>
2023-02-02 01:12:14 +0100

    configure Multer to save files with file extensions.

--- p2-e5/src/lib/middleware/multer.ts
+++ p2-e5/src/lib/middleware/multer.ts
@@ -1,7 +1,20 @@
+import { randomUUID } from "crypto";
+import mime from "mime";
 import multer from "multer";
+
+export const generatePhotoFilename = (mimeType: string) => {
+  const randomFilename = `${randomUUID()}-${Date.now()}`;
+  const fileExtension = mime.getExtension(mimeType);
+  const filename = `${randomFilename}.${fileExtension}`;
+
+  return filename;
+};
 
 const storage = multer.diskStorage({
   destination: "uploads/",
+  filename: (request, file, callback) => {
+    return callback(null, generatePhotoFilename(file.mimetype));
+  },
 });
 
 export const multerOptions = {};
